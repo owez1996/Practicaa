b8d6e2c919b4f3167d770bf26c6c32a948b7bb84
Sapar Hudaynazarow <hudaynazarow1515@gmail.com>
2021-04-23 13:47:52 +0300

    Sql query

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -10,12 +10,29 @@ public class Runner {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
 
-        List roles = null;
+        List<Object[]> list = null;
         try {
             session.beginTransaction();
 
-            Query query = session.createQuery("FROM Appeal ");
-            roles = query.list();
+            // Query<Object[]> query = session.createSQLQuery("SELECT id, round(cost/1.9556,2) AS Цена_в_USD, round (cost/2.3897,2) AS Цена_в_EUR, round(cost/3.4693*100,2) AS Цена_в_RUB FROM Conclusion\n");
+            Query<Object[]> query = session.createSQLQuery("SELECT Doctors.Name as Имя_доктора, Avg(Conclusion.Cost) as Цена_лечения, 'Мало' as Статус\n" +
+                    "FROM Doctors INNER JOIN Conclusion\n" +
+                    "ON Doctors.ID=Conclusion.ID_Doctor\n" +
+                    "WHERE Conclusion.Cost<100\n" +
+                    "GROUP BY Doctors.Name\n" +
+                    "UNION\n" +
+                    "SELECT Doctors.Name as Имя_доктора, Avg(Conclusion.Cost) as Цена_лечения, 'Средне' as Статус\n" +
+                    "FROM Doctors INNER JOIN Conclusion\n" +
+                    "ON Doctors.ID=Conclusion.ID_Doctor\n" +
+                    "WHERE Conclusion.Cost>=100 AND Conclusion.Cost<300\n" +
+                    "GROUP BY Doctors.Name\n" +
+                    "UNION\n" +
+                    "SELECT Doctors.Name as Имя_доктора, Avg(Conclusion.Cost) as Цена_лечения, 'Много' as Статус\n" +
+                    "FROM Doctors INNER JOIN Conclusion\n" +
+                    "ON Doctors.ID=Conclusion.ID_Doctor\n" +
+                    "WHERE Conclusion.Cost>=300\n" +
+                    "GROUP BY Doctors.Name");
+            list = query.list();
 
             session.getTransaction().commit();
         } catch (Exception e) {
@@ -25,7 +42,8 @@ public class Runner {
             session.close();
             sessionFactory.close();
         }
-        roles.forEach(System.out::println);
 
+        //list.forEach(p -> System.out.println("Id: "+p[0]+" Цена в USD: "+p[1] + " Цена в EUR: " + p[2] + " Цена в RUB: " + p[3]));
+        list.forEach(p -> System.out.println("Имя доктора: "+p[0]+" Цена лечения: "+p[1] + " Статус: " + p[2]));
     }
 }
--- src/main/java/entity/SuperID.java
+++ src/main/java/entity/SuperID.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Data
 @MappedSuperclass
@@ -14,4 +15,17 @@ public class SuperID implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SuperID superID = (SuperID) o;
+        return id == superID.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
